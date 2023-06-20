60e6c307d91c7cdac6e1b2e7f4185b31f0e6b8a0
lkdt <krumwiede@deltatest.de>
2023-06-20 06:46:22 +0200

    Generate inner XML content nodes if necessary
    A node like
    
        <Node>This has raw text</Node>
    
    will now get a field
    
        Content string `xml:",innerxml"`
    
    to capture the string "This has raw text".

--- main.go
+++ main.go
@@ -69,6 +69,20 @@ func generateCode(n Node) error {
 func convert(w io.Writer, n Node, parents []string) {
 	nodeName := goIdent(n.XMLName.Local)
 
+	// To not create two structs with the same name (which will be a compile
+	// error), we fully qualify all nodes with their complete hierarchy. For
+	// example these nodes:
+	//
+	//   <Top>
+	//     <Other/>
+	//     <SecondLevel>
+	//       <Other/>
+	//     <SecondLevel>
+	//   </Top>
+	//
+	// will generate structs `Top`, `Top_Other`, `Top_SecondLevel` and
+	// `Top_SecondLevel_Other`.
+	// If we just used the node name, `Other` would be created twice.
 	allNames := append(parents, nodeName)
 	qualifiedName := strings.Join(allNames, "_")
 
@@ -90,22 +104,54 @@ func convert(w io.Writer, n Node, parents []string) {
 		counts[name]++
 	}
 
+	// Create the Go struct for this node.
 	fmt.Fprintf(w, "type %s struct {\n", qualifiedName)
 	fmt.Fprintf(w, "\tXMLName xml.Name `xml:\"%s\"`\n", n.XMLName.Local)
+
+	// A node might contain raw text, like:
+	//
+	//   <Node>Some raw text</Node>
+	//
+	// and in that case we insert a Content field (hoping this name is not
+	// taken by any attribute or child-node).
+	//
+	// The way Go's XML parsing handles the ",innerxml" tag is that it will put
+	// all contents of a node in it, so a node like this:
+	//
+	//   <Node>
+	//     <SubNode/>
+	//   </Node>
+	//
+	// will contain this content: "\n    <SubNode/>\n  ". We simply get all
+	// content, even the XML nodes.
+	// Since we only want to add a Content string if there is raw textual
+	// content, we check if it starts with "<", in which case it is probably
+	// another node.
+	content := strings.TrimSpace(string(n.Content))
+	if !strings.HasPrefix(content, "<") {
+		fmt.Fprint(w, "\tContent string `xml:\",innerxml\"`\n")
+	}
+
+	// Write all attributes as strings. We do not convert to integers here,
+	// that can be done by the user.
 	for _, attr := range n.Attrs {
 		fmt.Fprintf(w, "\t%s string `xml:\"%s,attr\"`\n", goIdent(attr.Name.Local), attr.Name.Local)
 	}
+
+	// Write all child nodes. Some node names appear more than once, in that
+	// case we make the struct field a slice.
 	for _, n := range nodes {
-		array := ""
+		slice := ""
 		if counts[n.name] > 1 {
-			array = "[]"
+			slice = "[]"
 		}
 		typeName := goIdent(n.name)
 		qualifiedTypeName := qualifiedName + "_" + typeName
-		fmt.Fprintf(w, "\t%s %s%s `xml:\"%s\"`\n", typeName, array, qualifiedTypeName, n.name)
+		fmt.Fprintf(w, "\t%s %s%s `xml:\"%s\"`\n", typeName, slice, qualifiedTypeName, n.name)
 	}
 	fmt.Fprint(w, "}\n\n")
 
+	// Now create node structs for all the children of this node, recursively.
 	parents = append(parents, nodeName)
 	for _, n := range nodes {
 		convert(w, n.node, parents)
